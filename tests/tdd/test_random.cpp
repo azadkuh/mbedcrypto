e857ededa0eaa404371ffc1cdb6e5dbf4608c255
Amir Zamani <azadkuh@live.com>
2016-03-10 16:58:04 +0330

    tests/tdd: add random test unit

---
+++ tests/tdd/test_random.cpp
@@ -0,0 +1,35 @@
+#include <catch.hpp>
+#include <iostream>
+
+#include "mbedcrypto/random.hpp"
+#include "mbedcrypto/tcodec.hpp"
+///////////////////////////////////////////////////////////////////////////////
+namespace {
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+} // namespace anon
+///////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("random generator tests", "[random]") {
+    using namespace mbedcrypto;
+
+    mbedcrypto::random rnd("some custom text or binary data");
+
+    auto buf = rnd.make(128);
+    REQUIRE( buf.size() == 128 );
+
+    REQUIRE_NOTHROW( rnd.reseed() );
+    REQUIRE_NOTHROW( rnd.reseed("with some data") );
+    REQUIRE( rnd.reseed(nullptr, 0) == 0 );
+
+    REQUIRE_NOTHROW( rnd.update("with some data") );
+    REQUIRE_NOTHROW( rnd.update(std::string())    );
+
+    rnd.entropy_length(64);
+    rnd.reseed_interval(200);
+    rnd.prediction_resistance(true);
+
+    buf = rnd.make(93);
+    REQUIRE( buf.size() == 93 );
+}
