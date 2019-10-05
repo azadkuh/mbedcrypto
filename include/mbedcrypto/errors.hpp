0d7fc6f8079c5ce50ca9c53a5e5cfd19c9f036de
Amir Zamani <azadkuh@live.com>
2019-10-05 10:53:28 +0330

    add errors.hpp/cpp based on std::error_code

---
+++ include/mbedcrypto/errors.hpp
@@ -0,0 +1,61 @@
+/** @file errors.hpp
+ *
+ * @copyright (C) 2019
+ * @date 2019.10.02
+ * @author amir zamani <azadkuh@live.com>
+ *
+ */
+
+#ifndef MBEDCRYPTO_ERRORS_HPP
+#define MBEDCRYPTO_ERRORS_HPP
+
+#include <system_error>
+//-----------------------------------------------------------------------------
+namespace mbedcrypto {
+//-----------------------------------------------------------------------------
+namespace mbedtls {
+
+const std::error_category& error_category();
+
+inline auto
+error_message(int err) {
+    return error_category().message(err);
+}
+
+inline auto
+make_error_code(int err) {
+    return std::error_code{err, error_category()};
+}
+
+} // namespace mbedtls
+//-----------------------------------------------------------------------------
+
+enum class error_t : int {
+    success       = 0,   ///< success: no error
+    type          = 1,   ///< invalid or unknown type
+    usage         = 2,   ///< bad api call or invalid argument
+    not_supported = 10,  ///< not supported by this build
+    bad_hash      = 100, ///< invalid or unsupported hash type
+    bad_cipher    = 200, ///< invalid or unsupported cipher type
+    aead          = 210, ///< requires CCM or GCM modules, check build options
+    gcm           = 211, ///< requires CGM module, check build options
+    pk            = 500, ///< invalid or unsupported PK type
+    pk_export     = 501, ///< requires PE_EXPORT module, check build options
+    rsa_keygen    = 502, ///< requires RSA_KEYGEN, check build options
+    ecp           = 800, ///< invalid or unsupported EC (elliptic curve) type
+    unknown       = -1,  ///< unknown error
+}; // enum error_t
+
+//-----------------------------------------------------------------------------
+
+const std::error_category& error_category();
+
+inline auto
+make_error_code(error_t err) {
+    return std::error_code{static_cast<int>(err), error_category()};
+}
+
+//-----------------------------------------------------------------------------
+} // namespace mbedcrypto
+//-----------------------------------------------------------------------------
+#endif // MBEDCRYPTO_ERRORS_HPP
---
+++ src/errors.cpp
@@ -0,0 +1,95 @@
+#include "mbedcrypto/errors.hpp"
+
+#include <mbedtls/error.h>
+//-----------------------------------------------------------------------------
+namespace mbedcrypto {
+namespace {
+//-----------------------------------------------------------------------------
+
+/// mbedtls error codes
+struct mbedtls_category : std::error_category
+{
+    ~mbedtls_category() override = default;
+
+    const char* name() const noexcept override {
+        return "mbedtls";
+    }
+
+    std::string message(int err) const override {
+        std::string msg;
+        if (err != 0) {
+            constexpr size_t MaxSize           = 127;
+            char             buff[MaxSize + 1] = {0};
+            mbedtls_strerror(err, buff, MaxSize);
+            msg = buff;
+        }
+        return msg;
+    }
+}; // struct mbedtls_category
+
+//-----------------------------------------------------------------------------
+
+struct mbedcrypto_category : std::error_category
+{
+    ~mbedcrypto_category() override = default;
+
+    const char* name() const noexcept override {
+        return "mbedcrypto";
+    }
+
+    std::string message(int err) const override {
+        switch (err) {
+        case static_cast<int>(error_t::success):
+            return "success";
+        case static_cast<int>(error_t::type):
+            return "invalid or unknown type";
+        case static_cast<int>(error_t::usage):
+            return "bad api call or invalid argument";
+        case static_cast<int>(error_t::not_supported):
+            return "not supported by this build";
+        case static_cast<int>(error_t::bad_hash):
+            return "invalid or unsupported hash type";
+        case static_cast<int>(error_t::bad_cipher):
+            return "invalid or unsupported cipher type";
+        case static_cast<int>(error_t::aead):
+            return "requires CCM or GCM modules, check build options";
+        case static_cast<int>(error_t::gcm):
+            return "requires CGM module, check build options";
+        case static_cast<int>(error_t::pk):
+            return "invalid or unsupported PK type";
+        case static_cast<int>(error_t::pk_export):
+            return "requires PE_EXPORT module, check build options";
+        case static_cast<int>(error_t::rsa_keygen):
+            return "requires RSA_KEYGEN, check build options";
+        case static_cast<int>(error_t::ecp):
+            return "invalid or unsupported EC (elliptic curve) type";
+        default:
+            return "unknown error";
+        }
+    }
+
+}; // struct mbedcrypto_category
+
+//-----------------------------------------------------------------------------
+} // namespace anon
+//-----------------------------------------------------------------------------
+
+namespace mbedtls {
+const std::error_category&
+error_category() {
+    static mbedtls_category ecat;
+    return ecat;
+}
+} // namespace mbedtls
+
+//-----------------------------------------------------------------------------
+
+const std::error_category&
+error_category() {
+    static mbedcrypto_category ecat;
+    return ecat;
+}
+
+//-----------------------------------------------------------------------------
+} // namespace mbedcrypto
+//-----------------------------------------------------------------------------
