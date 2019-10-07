70ac3fdb858ef12efc48a10122aa92b1d929ab9a
Amir Zamani <azadkuh@live.com>
2019-10-07 12:01:02 +0330

    add a simple binary view for new api

---
+++ include/mbedcrypto/binutils.hpp
@@ -0,0 +1,67 @@
+/** @file binutils.hpp
+ *
+ * @copyright (C) 2019
+ * @date 2019.10.05
+ * @author amir zamani <azadkuh@live.com>
+ *
+ */
+
+#ifndef MBEDCRYPTO_BINUTILS_HPP
+#define MBEDCRYPTO_BINUTILS_HPP
+
+#include <string>
+
+#if defined(QT_CORE_LIB)
+#   include <QByteArray>
+#endif
+
+//-----------------------------------------------------------------------------
+namespace mbedcrypto {
+//-----------------------------------------------------------------------------
+
+/// std::string is able to hold both TEXT and Binary data.
+/// as encryption is frequently being used with both text strings and binaries,
+/// std::string is more convenient than std::vector<unsigned char> or
+/// std::basic_string<unsigned char>.
+/// although std::vector<unsigned char> is a better options for binary contents.
+using buffer_t = std::string;
+
+//-----------------------------------------------------------------------------
+
+/// a view for binary (or text) data.
+struct bin_view_t {
+    const uint8_t* data = nullptr;
+    size_t         size = 0;
+
+    bin_view_t() noexcept = default;
+
+    /// accepts char, unsigned char, ... or any other single-byte type
+    template <
+        typename T,
+        typename Size,
+        typename =
+            std::enable_if_t<sizeof(T) == 1 && std::is_integral<Size>::value>>
+    bin_view_t(const T* buffer, Size length) noexcept
+        : data{reinterpret_cast<const uint8_t*>(buffer)},
+          size{static_cast<size_t>(length)} {}
+
+    /// accepts any container with data() and size() member functions.
+    /// ex: std::string, std::span, std::array, std::vector, QByteArray, ...
+    template <
+        typename Container,
+        typename = std::enable_if_t<std::is_constructible<
+            bin_view_t,
+            decltype(std::declval<Container>().data()),
+            decltype(std::declval<Container>().size())>::value>>
+    bin_view_t(const Container& c) noexcept : bin_view_t{c.data(), c.size()} {}
+}; // struct bin_view_t
+
+inline bool
+is_empty(const bin_view_t& bv) noexcept {
+    return bv.size == 0 || bv.data == nullptr;
+}
+
+//-----------------------------------------------------------------------------
+} // namespace mbedcrypto
+//-----------------------------------------------------------------------------
+#endif // MBEDCRYPTO_BINUTILS_HPP
---
+++ tests/test_binutils.cpp
@@ -0,0 +1,53 @@
+#include <catch2/catch.hpp>
+
+#include "mbedcrypto/binutils.hpp"
+
+#include <vector>
+#include <list>
+#include <array>
+//-----------------------------------------------------------------------------
+namespace {
+using namespace mbedcrypto;
+//-----------------------------------------------------------------------------
+// valid pointer and size type
+static_assert(std::is_nothrow_constructible<bin_view_t, const char*,    int>::value,    "");
+static_assert(std::is_nothrow_constructible<bin_view_t, const uint8_t*, size_t>::value, "");
+static_assert(std::is_nothrow_constructible<bin_view_t, unsigned char*, short>::value,  "");
+
+// incompatible pointer or size type
+static_assert(!std::is_nothrow_constructible<bin_view_t, const short*, size_t>::value, "");
+static_assert(!std::is_nothrow_constructible<bin_view_t, const void*,  size_t>::value, "");
+static_assert(!std::is_nothrow_constructible<bin_view_t, const char*,  float>::value,  "");
+
+// valid containers
+static_assert(std::is_nothrow_constructible<bin_view_t,  std::string>::value,          "");
+static_assert(std::is_nothrow_constructible<bin_view_t,  std::vector<uint8_t>>::value, "");
+static_assert(std::is_nothrow_constructible<bin_view_t,  std::array<char, 8>>::value,  "");
+
+// incompatible containers
+static_assert(!std::is_nothrow_constructible<bin_view_t, std::wstring>::value,         "");
+static_assert(!std::is_nothrow_constructible<bin_view_t, std::vector<short>>::value,   "");
+static_assert(!std::is_nothrow_constructible<bin_view_t, std::list<uint8_t>>::value,   "");
+
+// is copy constructible and movable
+static_assert(std::is_nothrow_copy_constructible<bin_view_t>::value, "");
+static_assert(std::is_nothrow_move_constructible<bin_view_t>::value, "");
+
+//-----------------------------------------------------------------------------
+} // namespace anon
+//-----------------------------------------------------------------------------
+
+TEST_CASE("binary utils", "[binutils]") {
+    SECTION("empty") {
+        bin_view_t empty{};
+        REQUIRE(is_empty(empty));
+        REQUIRE(empty.data == nullptr);
+        REQUIRE(empty.size == 0);
+
+        const char* temp = nullptr;
+        REQUIRE(is_empty(bin_view_t{temp, 12}));
+        char buff[8] = {0};
+        REQUIRE(is_empty(bin_view_t{buff, 0}));
+        REQUIRE_FALSE(is_empty(bin_view_t{buff, 4}));
+    }
+}
