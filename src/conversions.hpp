3877e127551367c13ebaace4aa522515c6ccb386
Amir Zamani <azadkuh@live.com>
2019-10-07 16:16:16 +0330

    add conversion of mbedtls<->mbedcrypto into types.cpp

--- src/conversions.hpp
+++ src/conversions.hpp
@@ -1,4 +1,5 @@
 /** @file conversions.hpp
+ * implemented in types.cpp
  *
  * @copyright (C) 2016
  * @date 2016.03.04
@@ -8,7 +9,6 @@
 #ifndef MBEDCRYPTO_CONVERSIONS_HPP
 #define MBEDCRYPTO_CONVERSIONS_HPP
 
-#include "mbedcrypto_mbedtls_config.h"
 #include "mbedcrypto/types.hpp"
 
 #include <mbedtls/cipher.h>
@@ -17,23 +17,22 @@
 #include <mbedtls/pk.h>
 //-----------------------------------------------------------------------------
 namespace mbedcrypto {
-enum class cipher_bm;
 //-----------------------------------------------------------------------------
 // clang-format off
 
-auto to_native(hash_t)    -> mbedtls_md_type_t;
-auto to_native(cipher_t)  -> mbedtls_cipher_type_t;
-auto to_native(cipher_bm) -> mbedtls_cipher_mode_t;
-auto to_native(padding_t) -> mbedtls_cipher_padding_t;
-auto to_native(pk_t)      -> mbedtls_pk_type_t;
-auto to_native(curve_t)   -> mbedtls_ecp_group_id;
+mbedtls_md_type_t        to_native(hash_t) noexcept;
+mbedtls_cipher_padding_t to_native(padding_t) noexcept;
+mbedtls_cipher_mode_t    to_native(cipher_bm) noexcept;
+mbedtls_cipher_type_t    to_native(cipher_t) noexcept;
+mbedtls_pk_type_t        to_native(pk_t) noexcept;
+mbedtls_ecp_group_id     to_native(curve_t) noexcept;
 
-auto from_native(mbedtls_md_type_t)        -> hash_t;
-auto from_native(mbedtls_cipher_type_t)    -> cipher_t;
-auto from_native(mbedtls_cipher_mode_t)    -> cipher_bm;
-auto from_native(mbedtls_cipher_padding_t) -> padding_t;
-auto from_native(mbedtls_pk_type_t)        -> pk_t;
-auto from_native(mbedtls_ecp_group_id)     -> curve_t;
+hash_t                   from_native(mbedtls_md_type_t) noexcept;
+padding_t                from_native(mbedtls_cipher_padding_t) noexcept;
+cipher_bm                from_native(mbedtls_cipher_mode_t) noexcept;
+cipher_t                 from_native(mbedtls_cipher_type_t) noexcept;
+pk_t                     from_native(mbedtls_pk_type_t) noexcept;
+curve_t                  from_native(mbedtls_ecp_group_id) noexcept;
 
 // clang-format on
 //-----------------------------------------------------------------------------
--- src/types.cpp
+++ src/types.cpp
@@ -3,7 +3,7 @@
 // #include "mbedcrypto/pk.hpp"
 
 #include "./enumerator.hxx"
-// #include "./conversions.hpp"
+#include "./conversions.hpp"
 
 //-----------------------------------------------------------------------------
 namespace mbedcrypto {
@@ -121,6 +121,119 @@ const details::enum_name<curve_t> gCurves[] = {
     {curve_t::unknown,    "unknown"},
 };
 
+//-----------------------------------------------------------------------------
+
+const details::enum_pair<hash_t, mbedtls_md_type_t> gHashePairs[] = {
+    {hash_t::md2,       MBEDTLS_MD_MD2},
+    {hash_t::md4,       MBEDTLS_MD_MD4},
+    {hash_t::md5,       MBEDTLS_MD_MD5},
+    {hash_t::sha1,      MBEDTLS_MD_SHA1},
+    {hash_t::sha224,    MBEDTLS_MD_SHA224},
+    {hash_t::sha256,    MBEDTLS_MD_SHA256},
+    {hash_t::sha384,    MBEDTLS_MD_SHA384},
+    {hash_t::sha512,    MBEDTLS_MD_SHA512},
+    {hash_t::ripemd160, MBEDTLS_MD_RIPEMD160},
+    {hash_t::unknown,   MBEDTLS_MD_NONE},
+};
+
+const details::enum_pair<padding_t, mbedtls_cipher_padding_t> gPaddingPairs[] = {
+    {padding_t::none,          MBEDTLS_PADDING_NONE},
+    {padding_t::pkcs7,         MBEDTLS_PADDING_PKCS7},
+    {padding_t::one_and_zeros, MBEDTLS_PADDING_ONE_AND_ZEROS},
+    {padding_t::zeros_and_len, MBEDTLS_PADDING_ZEROS_AND_LEN},
+    {padding_t::zeros,         MBEDTLS_PADDING_ZEROS},
+    {padding_t::unknown,       MBEDTLS_PADDING_NONE},
+};
+
+const details::enum_pair<cipher_bm, mbedtls_cipher_mode_t> gBlockModePairs[] = {
+    {cipher_bm::ecb,     MBEDTLS_MODE_ECB},
+    {cipher_bm::cbc,     MBEDTLS_MODE_CBC},
+    {cipher_bm::cfb,     MBEDTLS_MODE_CFB},
+    {cipher_bm::ctr,     MBEDTLS_MODE_CTR},
+    {cipher_bm::gcm,     MBEDTLS_MODE_GCM},
+    {cipher_bm::stream,  MBEDTLS_MODE_STREAM},
+    {cipher_bm::ccm,     MBEDTLS_MODE_CCM},
+    {cipher_bm::unknown, MBEDTLS_MODE_NONE},
+};
+
+const details::enum_pair<cipher_t, mbedtls_cipher_type_t> gCipherPairs[] = {
+    {cipher_t::null,                MBEDTLS_CIPHER_NULL},
+    {cipher_t::aes_128_ecb,         MBEDTLS_CIPHER_AES_128_ECB},
+    {cipher_t::aes_192_ecb,         MBEDTLS_CIPHER_AES_192_ECB},
+    {cipher_t::aes_256_ecb,         MBEDTLS_CIPHER_AES_256_ECB},
+    {cipher_t::aes_128_cbc,         MBEDTLS_CIPHER_AES_128_CBC},
+    {cipher_t::aes_192_cbc,         MBEDTLS_CIPHER_AES_192_CBC},
+    {cipher_t::aes_256_cbc,         MBEDTLS_CIPHER_AES_256_CBC},
+    {cipher_t::aes_128_cfb128,      MBEDTLS_CIPHER_AES_128_CFB128},
+    {cipher_t::aes_192_cfb128,      MBEDTLS_CIPHER_AES_192_CFB128},
+    {cipher_t::aes_256_cfb128,      MBEDTLS_CIPHER_AES_256_CFB128},
+    {cipher_t::aes_128_ctr,         MBEDTLS_CIPHER_AES_128_CTR},
+    {cipher_t::aes_192_ctr,         MBEDTLS_CIPHER_AES_192_CTR},
+    {cipher_t::aes_256_ctr,         MBEDTLS_CIPHER_AES_256_CTR},
+    {cipher_t::aes_128_gcm,         MBEDTLS_CIPHER_AES_128_GCM},
+    {cipher_t::aes_192_gcm,         MBEDTLS_CIPHER_AES_192_GCM},
+    {cipher_t::aes_256_gcm,         MBEDTLS_CIPHER_AES_256_GCM},
+    {cipher_t::camellia_128_ecb,    MBEDTLS_CIPHER_CAMELLIA_128_ECB},
+    {cipher_t::camellia_192_ecb,    MBEDTLS_CIPHER_CAMELLIA_192_ECB},
+    {cipher_t::camellia_256_ecb,    MBEDTLS_CIPHER_CAMELLIA_256_ECB},
+    {cipher_t::camellia_128_cbc,    MBEDTLS_CIPHER_CAMELLIA_128_CBC},
+    {cipher_t::camellia_192_cbc,    MBEDTLS_CIPHER_CAMELLIA_192_CBC},
+    {cipher_t::camellia_256_cbc,    MBEDTLS_CIPHER_CAMELLIA_256_CBC},
+    {cipher_t::camellia_128_cfb128, MBEDTLS_CIPHER_CAMELLIA_128_CFB128},
+    {cipher_t::camellia_192_cfb128, MBEDTLS_CIPHER_CAMELLIA_192_CFB128},
+    {cipher_t::camellia_256_cfb128, MBEDTLS_CIPHER_CAMELLIA_256_CFB128},
+    {cipher_t::camellia_128_ctr,    MBEDTLS_CIPHER_CAMELLIA_128_CTR},
+    {cipher_t::camellia_192_ctr,    MBEDTLS_CIPHER_CAMELLIA_192_CTR},
+    {cipher_t::camellia_256_ctr,    MBEDTLS_CIPHER_CAMELLIA_256_CTR},
+    {cipher_t::camellia_128_gcm,    MBEDTLS_CIPHER_CAMELLIA_128_GCM},
+    {cipher_t::camellia_192_gcm,    MBEDTLS_CIPHER_CAMELLIA_192_GCM},
+    {cipher_t::camellia_256_gcm,    MBEDTLS_CIPHER_CAMELLIA_256_GCM},
+    {cipher_t::des_ecb,             MBEDTLS_CIPHER_DES_ECB},
+    {cipher_t::des_cbc,             MBEDTLS_CIPHER_DES_CBC},
+    {cipher_t::des_ede_ecb,         MBEDTLS_CIPHER_DES_EDE_ECB},
+    {cipher_t::des_ede_cbc,         MBEDTLS_CIPHER_DES_EDE_CBC},
+    {cipher_t::des_ede3_ecb,        MBEDTLS_CIPHER_DES_EDE3_ECB},
+    {cipher_t::des_ede3_cbc,        MBEDTLS_CIPHER_DES_EDE3_CBC},
+    {cipher_t::blowfish_ecb,        MBEDTLS_CIPHER_BLOWFISH_ECB},
+    {cipher_t::blowfish_cbc,        MBEDTLS_CIPHER_BLOWFISH_CBC},
+    {cipher_t::blowfish_cfb64,      MBEDTLS_CIPHER_BLOWFISH_CFB64},
+    {cipher_t::blowfish_ctr,        MBEDTLS_CIPHER_BLOWFISH_CTR},
+    {cipher_t::arc4_128,            MBEDTLS_CIPHER_ARC4_128},
+    {cipher_t::aes_128_ccm,         MBEDTLS_CIPHER_AES_128_CCM},
+    {cipher_t::aes_192_ccm,         MBEDTLS_CIPHER_AES_192_CCM},
+    {cipher_t::aes_256_ccm,         MBEDTLS_CIPHER_AES_256_CCM},
+    {cipher_t::camellia_128_ccm,    MBEDTLS_CIPHER_CAMELLIA_128_CCM},
+    {cipher_t::camellia_192_ccm,    MBEDTLS_CIPHER_CAMELLIA_192_CCM},
+    {cipher_t::camellia_256_ccm,    MBEDTLS_CIPHER_CAMELLIA_256_CCM},
+    {cipher_t::unknown,             MBEDTLS_CIPHER_NONE},
+};
+
+const details::enum_pair<pk_t, mbedtls_pk_type_t> gPkPairs[] = {
+    {pk_t::rsa,        MBEDTLS_PK_RSA},
+    {pk_t::eckey,      MBEDTLS_PK_ECKEY},
+    {pk_t::eckey_dh,   MBEDTLS_PK_ECKEY_DH},
+    {pk_t::ecdsa,      MBEDTLS_PK_ECDSA},
+    {pk_t::rsa_alt,    MBEDTLS_PK_RSA_ALT},
+    {pk_t::rsassa_pss, MBEDTLS_PK_RSASSA_PSS},
+    {pk_t::unknown,    MBEDTLS_PK_NONE},
+};
+
+const details::enum_pair<curve_t, mbedtls_ecp_group_id> gCurvePairs[] = {
+    {curve_t::secp192r1,  MBEDTLS_ECP_DP_SECP192R1},
+    {curve_t::secp224r1,  MBEDTLS_ECP_DP_SECP224R1},
+    {curve_t::secp256r1,  MBEDTLS_ECP_DP_SECP256R1},
+    {curve_t::secp384r1,  MBEDTLS_ECP_DP_SECP384R1},
+    {curve_t::secp521r1,  MBEDTLS_ECP_DP_SECP521R1},
+    {curve_t::secp192k1,  MBEDTLS_ECP_DP_SECP192K1},
+    {curve_t::secp224k1,  MBEDTLS_ECP_DP_SECP224K1},
+    {curve_t::secp256k1,  MBEDTLS_ECP_DP_SECP256K1},
+    {curve_t::bp256r1,    MBEDTLS_ECP_DP_BP256R1},
+    {curve_t::bp384r1,    MBEDTLS_ECP_DP_BP384R1},
+    {curve_t::bp512r1,    MBEDTLS_ECP_DP_BP512R1},
+    {curve_t::curve25519, MBEDTLS_ECP_DP_CURVE25519},
+    {curve_t::unknown,    MBEDTLS_ECP_DP_NONE},
+};
+
 // clang-format on
 //-----------------------------------------------------------------------------
 } // namespace anon
@@ -184,6 +297,68 @@ from_string(const char* name, pk_t& e) noexcept {
 void
 from_string(const char* name, curve_t& e) noexcept {
     e = details::from_string(name, gCurves);
+}
+
+//-----------------------------------------------------------------------------
+
+mbedtls_md_type_t
+to_native(hash_t e) noexcept {
+    return details::to_native(e, gHashePairs, MBEDTLS_MD_NONE);
+}
+
+mbedtls_cipher_padding_t
+to_native(padding_t e) noexcept {
+    return details::to_native(e, gPaddingPairs, MBEDTLS_PADDING_NONE);
+}
+
+mbedtls_cipher_mode_t
+to_native(cipher_bm e) noexcept {
+    return details::to_native(e, gBlockModePairs, MBEDTLS_MODE_NONE);
+}
+
+mbedtls_cipher_type_t
+to_native(cipher_t e) noexcept {
+    return details::to_native(e, gCipherPairs, MBEDTLS_CIPHER_NONE);
+}
+
+mbedtls_pk_type_t
+to_native(pk_t e) noexcept {
+    return details::to_native(e, gPkPairs, MBEDTLS_PK_NONE);
+}
+
+mbedtls_ecp_group_id
+to_native(curve_t e) noexcept {
+    return details::to_native(e, gCurvePairs, MBEDTLS_ECP_DP_NONE);
+}
+
+hash_t
+from_native(mbedtls_md_type_t n) noexcept {
+    return details::from_native(n, gHashePairs);
+}
+
+padding_t
+from_native(mbedtls_cipher_padding_t n) noexcept {
+    return details::from_native(n, gPaddingPairs);
+}
+
+cipher_bm
+from_native(mbedtls_cipher_mode_t n) noexcept {
+    return details::from_native(n, gBlockModePairs);
+}
+
+cipher_t
+from_native(mbedtls_cipher_type_t n) noexcept {
+    return details::from_native(n, gCipherPairs);
+}
+
+pk_t
+from_native(mbedtls_pk_type_t n) noexcept {
+    return details::from_native(n, gPkPairs);
+}
+
+curve_t
+from_native(mbedtls_ecp_group_id n) noexcept {
+    return details::from_native(n, gCurvePairs);
 }
 
 //-----------------------------------------------------------------------------
@@ -452,43 +627,6 @@ bool
 supports_curve(const char* name) {
     auto e = curve_from_string(name);
     return (e == curve_t::none) ? false : supports(e);
-}
-
-//-----------------------------------------------------------------------------
-
-hash_t
-hash_from_string(const char* name) {
-    auto t = mbedtls_md_get_type(
-        mbedtls_md_info_from_string(to_upper(name).c_str()));
-    return from_native(t);
-}
-
-padding_t
-padding_from_string(const char* name) {
-    return from_string<padding_t>(name, gPaddings);
-}
-
-cipher_bm
-block_mode_from_string(const char* name) {
-    return from_string<cipher_bm>(name, gBlockModes);
-}
-
-cipher_t
-cipher_from_string(const char* name) {
-    const auto* p = mbedtls_cipher_info_from_string(to_upper(name).c_str());
-    if (p == nullptr)
-        return cipher_t::none;
-    return from_native(p->type);
-}
-
-pk_t
-pk_from_string(const char* name) {
-    return from_string<pk_t>(name, gPks);
-}
-
-curve_t
-curve_from_string(const char* name) {
-    return from_string<curve_t>(name, gCurves);
 }
 
 #endif
