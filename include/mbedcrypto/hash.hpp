e5d3775baca63efeeee412e5fcb89f8a87975495
Amir Zamani <azadkuh@live.com>
2019-10-20 15:27:28 +0330

    implement new hash(message digest) interface

--- include/mbedcrypto/hash.hpp
+++ include/mbedcrypto/hash.hpp
@@ -9,182 +9,60 @@
 #define MBEDCRYPTO_HASH_HPP
 
 #include "mbedcrypto/types.hpp"
+#include "mbedcrypto/binutils.hpp"
+#include "mbedcrypto/errors.hpp"
+
 //-----------------------------------------------------------------------------
 namespace mbedcrypto {
 //-----------------------------------------------------------------------------
 
-/** a class for computing hash (message digest) values for buffer or files.
- *
- * related cmake build options:
- *  - MBEDCRYPTO_MD2
- *  - MBEDCRYPTO_MD4
- *  - MBEDCRYPTO_RIPEMD160
- *
- * sample:
- * @code
- *  hash sha1(hash_t::sha1);
- *  ...
- *  sha1.start();
- *  sha1.update(...);
- *  sha1.update(...);
- *  auto h1 = sha1.finish();
- *  sha1.start(); //start again
- *  sha1.update(...); // any updates ...
- *  auto h2 = sha1.finish();
- * @endcode
- *
- */
-class hash
+/// returns the length of a hash algorithm in byte or zero as error.
+size_t hash_size(hash_t) noexcept;
+
+/// makes the hash value for a buffer in single operation.
+/// the output and output_size should be large enough @sa hash_size()
+std::error_code
+make_hash(
+    bin_view_t input,
+    hash_t     algorithm,
+    uint8_t*   output,
+    size_t&    output_size) noexcept;
+
+/// makes the hash value for a file.
+/// the output and output_size should be large enough @sa hash_size()
+std::error_code
+make_file_hash(
+    const char* filename,
+    hash_t      algorithm,
+    uint8_t*    output,
+    size_t&     output_size) noexcept;
+
+//-----------------------------------------------------------------------------
+
+struct hash
 {
-public: // single-shot hash computation
-    /// returns the length of a hash algorithm in byte.
-    static size_t length(hash_t type);
-
-    /// makes the hash value for a buffer in single operation
-    static buffer_t
-    make(hash_t type, buffer_view_t src);
-
-    /// makes the hash value of a file content
-    static buffer_t of_file(hash_t type, const char* filePath);
-
-public: // iterative usage, reusing the instance
-    explicit hash(hash_t type); ///< throws if type is not supported
+    hash();
     ~hash();
 
-    size_t length() const noexcept;
+    hash(hash&&) noexcept = default;
+    hash& operator=(hash&&) noexcept = default;
 
-    /// resets and prepares the object to digest a new message.
-    void start();
+    std::error_code start(hash_t) noexcept;
+    std::error_code update(bin_view_t chunk) noexcept;
+    std::error_code finish(uint8_t* output, size_t& output_size) noexcept;
 
-    /** updates the hash by chunks of data.
-     * may be called repeatedly between start() and finish().
-     */
-    void update(const uint8_t* chunk, size_t chunk_size);
-
-    void update(buffer_view_t chunk) {
-        return update(chunk.data(), chunk.size());
+    template <class Container> auto finish(Container& output) {
+        size_t osize = 0;
+        finish(nullptr, osize);
+        output.resize(osize);
+        return finish(reinterpret_cast<uint8_t*>(&output[0]), osize);
     }
-
-    /// returns the final digest of previous updates.
-    buffer_t finish();
-
-    // this class is move-only
-    hash(const hash&) = delete;
-    hash(hash&&)      = default;
-    hash& operator=(const hash&) = delete;
-    hash& operator=(hash&&)      = default;
-
-#if defined(QT_CORE_LIB)
-    static QByteArray make(hash_t type, const QByteArray& src);
-#endif // QT_CORE_LIB
 
 protected:
     struct impl;
     std::unique_ptr<impl> pimpl;
-}; // hash
+}; // class hash
 
-//-----------------------------------------------------------------------------
-
-/** HMAC (hash-based message authentication code) implementation.
- * use the available hash algorithms to compute hmac value.
- *
- * sample:
- * @code
- *  hmac hms(hash_t::sha256);
- *  ...
- *  hms.start("an string or binary key"); // a key is mandatory for first time
- *  hms.update(...);
- *  hms.update(...);
- *  auto h1 = hms.finish();
- *  hms.start(); // do not change the previous key
- *  hms.update(...); // multiple updates ...
- *  hms.update(...);
- *  auto h2 = hms.finish();
- * @endcode
- *
- */
-class hmac
-{
-public: // single-shot hamc computation
-    /** makes a generic HMAC checksum by custom key.
-     * HMAC key could be of any size
-     */
-    static buffer_t make(hash_t type, buffer_view_t key, buffer_view_t src);
-
-public: // iterative or reuse
-    explicit hmac(hash_t type);
-    ~hmac();
-
-    /// resets and prepares the object to digest a new message.
-    void start(buffer_view_t key);
-    /// same as above, but does not change the previous key.
-    void start();
-
-
-    /** updates the hash by chunks of data.
-     * may be called repeatedly between start() and finish().
-     */
-    void update(const uint8_t* chunk, size_t chunk_size);
-
-    void update(buffer_view_t chunk) {
-        return update(chunk.data(), chunk.size());
-    }
-
-    /// returns the final digest of previous updates.
-    buffer_t finish();
-
-    // this class is move-only
-    hmac(const hmac&) = delete;
-    hmac(hmac&&)      = default;
-    hmac& operator=(const hmac&) = delete;
-    hmac& operator=(hmac&&)      = default;
-
-#if defined(QT_CORE_LIB)
-    static QByteArray
-    make(hash_t type, const QByteArray& key, const QByteArray& src);
-#endif // QT_CORE_LIB
-
-protected:
-    struct impl;
-    std::unique_ptr<impl> pimpl;
-}; // hmac
-
-//-----------------------------------------------------------------------------
-
-inline size_t
-hash_size(hash_t type) {
-    return hash::length(type);
-}
-
-template <class T>
-inline T
-make_hash(hash_t type, const T& src) {
-    return hash::make(type, src);
-}
-
-template<class T>
-inline T
-make_hmac(hash_t type, const T& key, const T& src) {
-    return hmac::make(type, key, src);
-}
-
-template <class T>
-inline T
-to_sha1(const T& src) {
-    return hash::make(hash_t::sha1, src);
-}
-
-template <class T>
-inline T
-to_sha256(const T& src) {
-    return hash::make(hash_t::sha256, src);
-}
-
-template <class T>
-inline T
-to_sha512(const T& src) {
-    return hash::make(hash_t::sha512, src);
-}
 
 //-----------------------------------------------------------------------------
 } // namespace mbedcrypto
--- src/hash.cpp
+++ src/hash.cpp
@@ -2,217 +2,145 @@
 #include "./conversions.hpp"
 
 #include <mbedtls/md.h>
-#include <tuple>
 #include <type_traits>
 //-----------------------------------------------------------------------------
 namespace mbedcrypto {
 namespace {
 //-----------------------------------------------------------------------------
-
 static_assert(std::is_copy_constructible<hash>::value == false, "");
-static_assert(std::is_move_constructible<hash>::value == true, "");
-static_assert(std::is_copy_constructible<hmac>::value == false, "");
-static_assert(std::is_move_constructible<hmac>::value == true, "");
+static_assert(std::is_move_constructible<hash>::value == true,  "");
+static_assert(std::is_copy_assignable<hash>::value    == false, "");
+static_assert(std::is_move_assignable<hash>::value    == true,  "");
+
+//-----------------------------------------------------------------------------
 
 const mbedtls_md_info_t*
-native_type(hash_t type) {
-    const auto* info = mbedtls_md_info_from_type(to_native(type));
-    if (info == nullptr)
-        throw exceptions::unknown_hash{};
-
-    return info;
-}
-
-auto
-digest_pair(hash_t type) {
-    const auto* cinfot = native_type(type);
-    size_t      length = mbedtls_md_get_size(cinfot);
-
-    return std::make_tuple(cinfot, length);
-}
-
-template <class T, class Tuple>
-T
-_zero_initialized_buffer(const Tuple& p) {
-    using length_t = decltype(T().size());
-    return T(static_cast<length_t>(std::get<1>(p)), '\0');
-}
-
-template <class T>
-T
-_make(hash_t type, buffer_view_t src) {
-    auto digest = digest_pair(type);
-    auto buf    = _zero_initialized_buffer<T>(digest);
-
-    mbedcrypto_c_call(
-        mbedtls_md, std::get<0>(digest), src.data(), src.size(), to_ptr(buf));
-
-    return buf;
-}
-
-template <class T>
-T
-_make(hash_t type, buffer_view_t key, buffer_view_t src) {
-    auto digest = digest_pair(type);
-    auto buf    = _zero_initialized_buffer<T>(digest);
-
-    mbedcrypto_c_call(
-        mbedtls_md_hmac,
-        std::get<0>(digest),
-        key.data(),
-        key.size(),
-        src.data(),
-        src.size(),
-        to_ptr(buf));
-
-    return buf;
+find_native_info(hash_t algo) noexcept {
+    if (algo == hash_t::unknown)
+        return nullptr;
+    return mbedtls_md_info_from_type(to_native(algo));
 }
 
 //-----------------------------------------------------------------------------
 
-struct impl_base {
-    mbedtls_md_context_t ctx_;
+struct impl_base
+{
+    mbedtls_md_context_t ctx;
 
-    explicit impl_base() noexcept {
-        mbedtls_md_init(&ctx_);
-    }
+    void init() noexcept  { mbedtls_md_init(&ctx); }
+    void reset() noexcept { mbedtls_md_free(&ctx); }
 
-    ~impl_base() {
-        mbedtls_md_free(&ctx_);
-    }
+    explicit impl_base() noexcept { init(); }
+    ~impl_base() { reset(); }
 
-    void setup(hash_t type, bool hmac) {
-        const auto* cinfot = native_type(type);
-        mbedcrypto_c_call(mbedtls_md_setup, &ctx_, cinfot, (hmac) ? 1 : 0);
+    int setup(const mbedtls_md_info_t* info , bool hmac) noexcept {
+        if (ctx.md_info && info && info != ctx.md_info) {
+            // it's a new algorithm
+            reset();
+            init();
+        }
+        return mbedtls_md_setup(&ctx, info, hmac ? 1 : 0);
     }
 
     size_t size() const noexcept {
-        if (ctx_.md_info == nullptr)
-            return 0;
-
-        return mbedtls_md_get_size(ctx_.md_info);
+        return ctx.md_info ? mbedtls_md_get_size(ctx.md_info) : 0;
     }
-
 }; // struct impl_base
 
 //-----------------------------------------------------------------------------
 } // namespace anon
 //-----------------------------------------------------------------------------
 
-struct hash::impl : public impl_base {};
-struct hmac::impl : public impl_base {};
+struct hash::impl : impl_base{};
 
 //-----------------------------------------------------------------------------
 
-hash::hash(hash_t type) : pimpl(new hash::impl) {
-    pimpl->setup(type, false);
-}
-
-hash::~hash() {}
-
-hmac::hmac(hash_t type) : pimpl(new hmac::impl) {
-    pimpl->setup(type, true);
-}
-
-hmac::~hmac() {}
-
 size_t
-hash::length(hash_t type) {
-    const auto* cinfot = native_type(type);
-    return mbedtls_md_get_size(cinfot);
+hash_size(hash_t algo) noexcept {
+    return mbedtls_md_get_size(find_native_info(algo));
 }
 
-size_t
-hash::length() const noexcept {
-    return pimpl->size();
+std::error_code
+make_hash(
+    bin_view_t input, hash_t algo, uint8_t* output, size_t& osize) noexcept {
+    const auto  capacity = osize;
+    const auto* info     = find_native_info(algo);
+    osize                = mbedtls_md_get_size(info);
+    if (osize == 0) {
+        return make_error_code(error_t::bad_hash);
+    } else if (output == nullptr || capacity == 0) {
+    } else if (capacity < osize) {
+        return make_error_code(error_t::small_output);
+    } else {
+        int ret = mbedtls_md(info, input.data, input.size, output);
+        if (ret != 0)
+            return mbedtls::make_error_code(ret);
+    }
+    return std::error_code{};
 }
 
-buffer_t
-hash::make(hash_t type, buffer_view_t src) {
-    return _make<buffer_t>(type, src);
-}
-
-buffer_t
-hash::of_file(hash_t type, const char* filePath) {
-#if defined(MBEDTLS_FS_IO)
-    auto digest = digest_pair(type);
-    buffer_t buf(std::get<1>(digest), '\0');
-
-    mbedcrypto_c_call(
-        mbedtls_md_file,
-        std::get<0>(digest),
-        filePath,
-        to_ptr(buf));
-
-    return buf;
-
+std::error_code
+make_file_hash(
+    const char* fname, hash_t algo, uint8_t* output, size_t& osize) noexcept {
+#if !defined(MBEDTLS_FS_IO)
+    osize = 0;
+    return make_error_code(error_t::not_supported);
 #else
-    throw support_exception{};
-
+    const auto  capacity = osize;
+    const auto* info     = find_native_info(algo);
+    osize                = mbedtls_md_get_size(info);
+    if (osize == 0) {
+        return make_error_code(error_t::bad_hash);
+    } else if (output == nullptr || capacity == 0) {
+    } else if (capacity < osize) {
+        return make_error_code(error_t::small_output);
+    } else {
+        int ret = mbedtls_md_file(info, fname, output);
+        if (ret != 0)
+            return mbedtls::make_error_code(ret);
+    }
+    return std::error_code{};
 #endif
 }
 
-buffer_t
-hmac::make(hash_t type, buffer_view_t key, buffer_view_t src) {
-    return _make<buffer_t>(type, key, src);
-}
-
-void
-hash::start() {
-    mbedcrypto_c_call(mbedtls_md_starts, &pimpl->ctx_);
-}
-
-void
-hash::update(const uint8_t* src, size_t length) {
-    mbedcrypto_c_call(mbedtls_md_update, &pimpl->ctx_, src, length);
-}
-
-buffer_t
-hash::finish() {
-    buffer_t digest(pimpl->size(), '\0');
-    mbedcrypto_c_call(mbedtls_md_finish, &pimpl->ctx_, to_ptr(digest));
-
-    return digest;
-}
-
-void
-hmac::start(buffer_view_t key) {
-    mbedcrypto_c_call(
-        mbedtls_md_hmac_starts, &pimpl->ctx_, key.data(), key.size());
-}
-
-void
-hmac::start() {
-    mbedcrypto_c_call(mbedtls_md_hmac_reset, &pimpl->ctx_);
-}
-
-void
-hmac::update(const uint8_t* src, size_t length) {
-    mbedcrypto_c_call(mbedtls_md_hmac_update, &pimpl->ctx_, src, length);
-}
-
-buffer_t
-hmac::finish() {
-    buffer_t digest(pimpl->size(), '\0');
-    mbedcrypto_c_call(mbedtls_md_hmac_finish, &pimpl->ctx_, to_ptr(digest));
-
-    return digest;
-}
-
 //-----------------------------------------------------------------------------
-#if defined(QT_CORE_LIB)
-//-----------------------------------------------------------------------------
-QByteArray
-hash::make(hash_t type, const QByteArray& src) {
-    return _make<QByteArray>(type, src);
+
+hash::hash() : pimpl{new impl} {}
+hash::~hash() = default;
+
+std::error_code
+hash::start(hash_t algo) noexcept {
+    const auto* ntype = find_native_info(algo);
+    if (ntype == nullptr)
+        return make_error_code(error_t::bad_hash);
+    int ret = pimpl->setup(ntype, false);
+    if (ret != 0)
+        return mbedtls::make_error_code(ret);
+    ret = mbedtls_md_starts(&pimpl->ctx);
+    return mbedtls::make_error_code(ret);
 }
 
-QByteArray
-hmac::make(hash_t type, const QByteArray& key, const QByteArray& src) {
-    return _make<QByteArray>(type, key, src);
+std::error_code
+hash::update(bin_view_t input) noexcept {
+    int ret = mbedtls_md_update(&pimpl->ctx, input.data, input.size);
+    return mbedtls::make_error_code(ret);
 }
-//-----------------------------------------------------------------------------
-#endif // QT_CORE_LIB
+
+std::error_code
+hash::finish(uint8_t* output, size_t& osize) noexcept {
+    const auto capacity = osize;
+    osize               = pimpl->size();
+    if (output == nullptr || capacity == 0) {
+    } else if (capacity < osize) {
+        return make_error_code(error_t::small_output);
+    } else {
+        int ret = mbedtls_md_finish(&pimpl->ctx, output);
+        if (ret != 0)
+            return mbedtls::make_error_code(ret);
+    }
+    return std::error_code{};
+}
+
 //-----------------------------------------------------------------------------
 } // namespace mbedcrypto
 //-----------------------------------------------------------------------------
