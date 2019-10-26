f9d5982e521c36b14d37e1e47e9f17ce592b52f5
Amir Zamani <azadkuh@live.com>
2019-10-26 20:18:15 +0330

    tests: add one-shot en-decryption to cipher tests

--- tests/test_cipher.cpp
+++ tests/test_cipher.cpp
@@ -96,26 +96,103 @@ const cipher_properties Props[] = {
     {cipher_t::chacha20_poly1305,   1,  12, 256, cipher_bm::chachapoly},
 };
 
+auto
+padding_of(cipher_bm bm) noexcept {
+    return bm != cipher_bm::cbc ? padding_t::none : padding_t::pkcs7;
+}
+
+bin_view_t
+make_source(bin_view_t in, const cipher_properties& p) noexcept {
+    auto copy{in};
+    if (p.bmode == cipher_bm::ecb)
+        copy.size = (p.block_size); // at the moment only block_size is supported for ECB
+        // copy.size -= (in.size % p.block_size);
+    return copy;
+}
+
+//-----------------------------------------------------------------------------
+
+struct tester {
+    explicit tester(const cipher_properties& p) noexcept : prop{p} {}
+
+    void run() const {
+        check_props();
+        one_shots();
+    }
+
+protected:
+    cipher_properties prop;
+
+    void check_props() const {
+        const auto bsize = block_size(prop.type);
+        const auto isize = iv_size(prop.type);
+        const auto kbits = key_bitlen(prop.type);
+        const auto bmode = block_mode(prop.type);
+        REQUIRE(bsize == prop.block_size);
+        REQUIRE(isize == prop.iv_size);
+        REQUIRE(kbits == prop.key_bits);
+        REQUIRE(bmode == prop.bmode);
+        cipher::info_t ci;
+        ci.type = prop.type;
+        ci.key  = test::long_binary();
+        ci.iv   = test::long_binary();
+        REQUIRE_FALSE(is_valid(ci)); // bad key/iv size
+        ci.key.size = kbits >> 3; // in bytes
+        ci.iv.size  = isize;      // in bytes
+        REQUIRE(is_valid(ci));
+    }
+
+    void one_shots() const {
+        if (prop.bmode == cipher_bm::ccm) // CCM is only for AEAD
+            return;
+        cipher::info_t ci;
+        ci.type    = prop.type;
+        ci.padding = padding_of(prop.bmode);
+        ci.key     = test::short_binary();
+        ci.iv      = test::short_text();
+        REQUIRE(ci.iv.size > prop.iv_size);
+        ci.iv.size  = prop.iv_size;
+        ci.key.size = prop.key_bits >> 3; // to byte
+
+        const auto source = make_source(test::long_text(), prop);
+
+        std::vector<uint8_t> enc;
+        auto ec = cipher::encrypt(obuffer_t{enc}, source, ci);
+        REQUIRE_FALSE(ec);
+        REQUIRE(enc.size() >= source.size);
+
+        std::string dec;
+        ec = cipher::decrypt(obuffer_t{dec}, enc, ci);
+        REQUIRE_FALSE(ec);
+
+        REQUIRE(dec == source);
+    }
+};
+
 //-----------------------------------------------------------------------------
 } // namespace anon
 //-----------------------------------------------------------------------------
 
 TEST_CASE("cipher properties", "[cipher]") {
-    for (const auto& p : Props) {
-        const auto bsize = block_size(p.type);
-        const auto isize = iv_size(p.type);
-        const auto kbits = key_bitlen(p.type);
-        const auto bmode = block_mode(p.type);
-        if (supports(p.type)) {
-            REQUIRE(bsize == p.block_size);
-            REQUIRE(isize == p.iv_size);
-            REQUIRE(kbits == p.key_bits);
-            REQUIRE(bmode == p.bmode);
-        } else {
-            REQUIRE(bsize == 0);
-            REQUIRE(isize == 0);
-            REQUIRE(kbits == 0);
-            REQUIRE(bmode == cipher_bm::unknown);
+    SECTION("empty ciphers") {
+        cipher::info_t ci;
+        REQUIRE_FALSE(is_valid(ci));
+        REQUIRE(block_size(ci.type) == 0);
+        REQUIRE(iv_size(ci.type)    == 0);
+        REQUIRE(key_bitlen(ci.type) == 0);
+        REQUIRE(block_mode(ci.type) == cipher_bm::unknown);
+    }
+
+    SECTION("all supported ciphers") {
+        for (const auto& p : Props) {
+            if (supports(p.type)) {
+                tester{p}.run();
+            } else {
+                REQUIRE(block_size(p.type) == 0);
+                REQUIRE(iv_size(p.type)    == 0);
+                REQUIRE(key_bitlen(p.type) == 0);
+                REQUIRE(block_mode(p.type) == cipher_bm::unknown);
+            }
         }
     }
 }
