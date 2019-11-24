1b2d364beb515f79d22e57e09703e6b3d2ebd448
Amir Zamani <azadkuh@live.com>
2019-11-24 16:08:13 +0330

    tests: refactor pk tests by new supports()

--- tests/test_pk.cpp
+++ tests/test_pk.cpp
@@ -54,83 +54,85 @@ TEST_CASE("private api tests", "[pk]") {
     }
 
     SECTION("setup ec") {
-        #if defined(MBEDCRYPTO_PK_EC) && defined(MBEDCRYPTO_PK_KEYGEN)
-        auto ec = pk::setup(ctx, pk_t::eckey);
-        REQUIRE_FALSE(ec);
-        REQUIRE(pk::type_of(ctx) == pk_t::eckey);
+        if (supports(features::pk_ec)) {
+            auto ec = pk::setup(ctx, pk_t::eckey);
+            REQUIRE_FALSE(ec);
+            REQUIRE(pk::type_of(ctx) == pk_t::eckey);
 
-        ec = pk::setup(ctx, pk_t::eckey_dh);
-        REQUIRE_FALSE(ec);
-        REQUIRE(pk::type_of(ctx) == pk_t::eckey_dh);
+            ec = pk::setup(ctx, pk_t::eckey_dh);
+            REQUIRE_FALSE(ec);
+            REQUIRE(pk::type_of(ctx) == pk_t::eckey_dh);
 
-        ec = pk::setup(ctx, pk_t::ecdsa);
-        REQUIRE_FALSE(ec);
-        REQUIRE(pk::type_of(ctx) == pk_t::ecdsa);
-        #else
-        auto ec = pk::setup(ctx, pk_t::eckey);
-        REQUIRE(ec == make_error_code(error_t::not_supported));
-        REQUIRE(pk::type_of(ctx) == pk_t::unknown);
+            ec = pk::setup(ctx, pk_t::ecdsa);
+            REQUIRE_FALSE(ec);
+            REQUIRE(pk::type_of(ctx) == pk_t::ecdsa);
+        } else {
+            auto ec = pk::setup(ctx, pk_t::eckey);
+            REQUIRE(ec == make_error_code(error_t::not_supported));
+            REQUIRE(pk::type_of(ctx) == pk_t::unknown);
 
-        ec = pk::setup(ctx, pk_t::eckey_dh);
-        REQUIRE(ec == make_error_code(error_t::not_supported));
-        REQUIRE(pk::type_of(ctx) == pk_t::unknown);
+            ec = pk::setup(ctx, pk_t::eckey_dh);
+            REQUIRE(ec == make_error_code(error_t::not_supported));
+            REQUIRE(pk::type_of(ctx) == pk_t::unknown);
 
-        ec = pk::setup(ctx, pk_t::ecdsa);
-        REQUIRE(ec == make_error_code(error_t::not_supported));
-        REQUIRE(pk::type_of(ctx) == pk_t::unknown);
-        #endif
+            ec = pk::setup(ctx, pk_t::ecdsa);
+            REQUIRE(ec == make_error_code(error_t::not_supported));
+            REQUIRE(pk::type_of(ctx) == pk_t::unknown);
+        }
     }
 
     SECTION("make rsa key") {
-        #if defined(MBEDCRYPTO_PK_KEYGEN)
         constexpr size_t keybits = 1024;
         auto ec = make_rsa_key(ctx, keybits);
-        REQUIRE_FALSE(ec);
-        REQUIRE(pk::type_of(ctx)                  == pk_t::rsa);
-        REQUIRE(pk::key_bitlen(ctx)               == keybits);
-        REQUIRE(pk::key_size(ctx)                 == 128); // 1024 / 8
-        REQUIRE(pk::max_crypt_size(ctx)           == (128 - 11));
-        REQUIRE(pk::has_private_key(ctx)          == true);
-        REQUIRE(pk::can_do(ctx, pk_t::rsa)        == true);
-        REQUIRE(pk::can_do(ctx, pk_t::rsa_alt)    == false);
-        REQUIRE(pk::can_do(ctx, pk_t::rsassa_pss) == true);
-        REQUIRE(pk::can_do(ctx, pk_t::eckey)      == false);
-        REQUIRE(pk::can_do(ctx, pk_t::eckey_dh)   == false);
-        REQUIRE(pk::can_do(ctx, pk_t::ecdsa)      == false);
-        auto cap = pk::what_can_do(ctx);
-        REQUIRE(cap.encrypt == true);
-        REQUIRE(cap.decrypt == true);
-        REQUIRE(cap.sign    == true);
-        REQUIRE(cap.verify  == true);
+        if (supports(features::pk_keygen)) {
+            REQUIRE_FALSE(ec);
+            REQUIRE(pk::type_of(ctx)                  == pk_t::rsa);
+            REQUIRE(pk::key_bitlen(ctx)               == keybits);
+            REQUIRE(pk::key_size(ctx)                 == 128); // 1024 / 8
+            REQUIRE(pk::max_crypt_size(ctx)           == (128 - 11));
+            REQUIRE(pk::has_private_key(ctx)          == true);
+            REQUIRE(pk::can_do(ctx, pk_t::rsa)        == true);
+            REQUIRE(pk::can_do(ctx, pk_t::rsa_alt)    == false);
+            REQUIRE(pk::can_do(ctx, pk_t::rsassa_pss) == true);
+            REQUIRE(pk::can_do(ctx, pk_t::eckey)      == false);
+            REQUIRE(pk::can_do(ctx, pk_t::eckey_dh)   == false);
+            REQUIRE(pk::can_do(ctx, pk_t::ecdsa)      == false);
+            auto cap = pk::what_can_do(ctx);
+            REQUIRE(cap.encrypt == true);
+            REQUIRE(cap.decrypt == true);
+            REQUIRE(cap.sign    == true);
+            REQUIRE(cap.verify  == true);
 
-        std::string pub_data;
-        ec = pk::export_pub_key(pub_data, ctx, pk::key_io_t::der);
-        REQUIRE_FALSE(ec);
-        REQUIRE_FALSE(pub_data.empty());
+            std::string pub_data;
+            ec = pk::export_pub_key(pub_data, ctx, pk::key_io_t::der);
+            REQUIRE_FALSE(ec);
+            REQUIRE_FALSE(pub_data.empty());
 
-        pk::context pub;
-        ec = pk::import_pub_key(pub, pub_data);
-        REQUIRE_FALSE(ec);
-        REQUIRE(pk::is_valid(pub));
-        REQUIRE(is_pri_pub_pair(ctx, pub));
+            pk::context pub;
+            ec = pk::import_pub_key(pub, pub_data);
+            REQUIRE_FALSE(ec);
+            REQUIRE(pk::is_valid(pub));
+            REQUIRE(is_pri_pub_pair(ctx, pub));
 
-        REQUIRE(pk::type_of(pub)                  == pk_t::rsa);
-        REQUIRE(pk::key_bitlen(pub)               == keybits);
-        REQUIRE(pk::key_size(pub)                 == 128); // 1024 / 8
-        REQUIRE(pk::max_crypt_size(pub)           == (128 - 11));
-        REQUIRE(pk::has_private_key(pub)          == false);
-        REQUIRE(pk::can_do(pub, pk_t::rsa)        == true);
-        REQUIRE(pk::can_do(pub, pk_t::rsa_alt)    == false);
-        REQUIRE(pk::can_do(pub, pk_t::rsassa_pss) == true);
-        REQUIRE(pk::can_do(pub, pk_t::eckey)      == false);
-        REQUIRE(pk::can_do(pub, pk_t::eckey_dh)   == false);
-        REQUIRE(pk::can_do(pub, pk_t::ecdsa)      == false);
-        cap = pk::what_can_do(pub);
-        REQUIRE(cap.encrypt == true);
-        REQUIRE(cap.decrypt == false);
-        REQUIRE(cap.sign    == false);
-        REQUIRE(cap.verify  == true);
-        #endif
+            REQUIRE(pk::type_of(pub)                  == pk_t::rsa);
+            REQUIRE(pk::key_bitlen(pub)               == keybits);
+            REQUIRE(pk::key_size(pub)                 == 128); // 1024 / 8
+            REQUIRE(pk::max_crypt_size(pub)           == (128 - 11));
+            REQUIRE(pk::has_private_key(pub)          == false);
+            REQUIRE(pk::can_do(pub, pk_t::rsa)        == true);
+            REQUIRE(pk::can_do(pub, pk_t::rsa_alt)    == false);
+            REQUIRE(pk::can_do(pub, pk_t::rsassa_pss) == true);
+            REQUIRE(pk::can_do(pub, pk_t::eckey)      == false);
+            REQUIRE(pk::can_do(pub, pk_t::eckey_dh)   == false);
+            REQUIRE(pk::can_do(pub, pk_t::ecdsa)      == false);
+            cap = pk::what_can_do(pub);
+            REQUIRE(cap.encrypt == true);
+            REQUIRE(cap.decrypt == false);
+            REQUIRE(cap.sign    == false);
+            REQUIRE(cap.verify  == true);
+        } else {
+            REQUIRE(ec == make_error_code(error_t::not_supported));
+        }
     }
 }
 
